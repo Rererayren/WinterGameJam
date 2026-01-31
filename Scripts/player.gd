bb356687f6c9c53518e2f308b0dc8710927cd190
Han Tran <hantr060902@gmail.com>
2026-01-31 14:24:32 -0600

    Add stamina, double jump. and sprint

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,11 +3,16 @@ extends CharacterBody2D
 const MAX_SPEED: float = 64.5
 const SPRINT: float = MAX_SPEED * 2.0
 const JUMP_HEIGHT: float = -165.0
-const GRAVITY: float = 12.5
+const MAX_JUMP_TRIES: int = 2
+const GRAVITY: float = 9.81
 const ACCELERATION: float = 18.5
 const FRICTION: float = 22.5
+const MAX_STAMINA: float = 100.0
+const STAMINA_REGEN: float = 5.0
 
 var look_dir_x: int = 1
+
+var jump_count: int = 0
 
 const DASH_SPEED: float = 200.0
 const DASH_TIME: float = 0.12
@@ -17,6 +22,8 @@ var dash_timer: float = 0.0
 
 const SUPER_DASH_SPEED: float = 300.0
 const SUPER_DASH_TIME: float = 0.2
+const SUPER_DASH_COST: float = 20.0
+var stamina = MAX_STAMINA
 var super_dash_unlocked: bool = true
 var can_super_dash: bool = true
 var is_super_dashing: bool = false
@@ -33,14 +40,17 @@ func _physics_process(delta: float) -> void:
 	if dash_timer == 0.0 and super_dash_charge_timer == 0.0:
 		var velocity_weight_x: float = 1.0 - exp(-(ACCELERATION if dir_x else FRICTION) * delta)
 		velocity.x = lerp(velocity.x, dir_x * MAX_SPEED, velocity_weight_x)
+		_sprint_logic(delta, dir_x)
 	
 		if dir_x:
 			look_dir_x = int(dir_x)
 	
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_HEIGHT
+		_double_jump_logic(delta)
 		velocity.y += GRAVITY
 	
+	_regen_stamina(delta)
 	_handle_dash_joypad_priority(delta)
 	if dash_unlocked:
 		_dash_logic(delta)
@@ -54,6 +64,18 @@ func _physics_process(delta: float) -> void:
 			can_super_dash = true
 	
 	move_and_slide()
+
+func _sprint_logic(delta: float, direction: float) -> void:
+	if Input.is_action_pressed("sprint"):
+		velocity.x = SPRINT * direction
+
+func _double_jump_logic(delta: float) -> void:
+	if is_on_floor():
+		jump_count = 0
+	if Input.is_action_just_pressed("jump"):
+		jump_count += 1
+		if jump_count <= MAX_JUMP_TRIES:
+			velocity.y = JUMP_HEIGHT
 	
 func _dash_logic(delta: float) -> void:
 	if can_dash and Input.is_action_just_pressed("dash") and not suppres_dash:
@@ -68,11 +90,16 @@ func _dash_logic(delta: float) -> void:
 			dash_timer = 0.0
 	
 func _super_dash_logic(delta: float) -> void:
-	if can_super_dash and is_on_floor() and not is_super_dashing:
+	if stamina < SUPER_DASH_COST:
+		super_dash_charge_cost = 0.0
+		return
+	
+	if can_super_dash and stamina >= SUPER_DASH_COST and is_on_floor() and not is_super_dashing:
 		if Input.is_action_pressed("super_dash") and suppres_dash:
 			velocity = Vector2.ZERO
 			super_dash_charge_timer += delta
 			if super_dash_charge_timer >= super_dash_charge_cost:
+				stamina -= SUPER_DASH_COST
 				can_super_dash = false
 				is_super_dashing = true
 				super_dash_timer = SUPER_DASH_TIME
@@ -101,3 +128,9 @@ func _handle_dash_joypad_priority(delta: float) -> void:
 	if Input.is_action_just_released("dash"):
 		dash_hold_time = 0.0
 		suppres_dash = false
+
+func _regen_stamina(delta: float) -> void:
+	if stamina < MAX_STAMINA:
+		stamina += STAMINA_REGEN * delta
+		stamina = min(stamina, MAX_STAMINA)
+		print(stamina)
