7a3cc1c5d2f5e4338d574e4e5ca0f95e815ef62f
Khang Duong <sugarkhang02@gmail.com>
2026-01-30 20:06:06 -0600

    first commit

---
+++ Scripts/area_2d_killzone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+#drag and then select ctrl
+@onready var timer: Timer = $Timer #path to nodee
+
+#only drag
+#$Timer
+
+func _on_body_entered(body: Node2D) -> void:
+	print("you died!!!")
+	timer.start
+
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
